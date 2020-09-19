b373974809b34edab1afeb9073ba52f5e9be1a31
Sukaina Ahmed Yahya Al-Mutawakel <sukaina2099@gmail.com>
2020-09-19 23:15:30 +0300

    initalization

--- app/src/main/java/com/example/androidquestion2third/MainActivity.kt
+++ app/src/main/java/com/example/androidquestion2third/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-   // private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -21,14 +23,15 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true)
     )
-    private var currentIndex = 0
+    private var currentIndex =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        //nextButton = findViewById(R.id.next_button)
+        nextButton = findViewById(R.id.next_button)
+        previousButton=findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -45,10 +48,18 @@ class MainActivity : AppCompatActivity() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
 
-        questionTextView.setOnClickListener {
+        nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
 
+        }
+
+        previousButton.setOnClickListener {
+
+            currentIndex = (currentIndex - 1) % questionBank.size
+            if(currentIndex<0)
+                currentIndex=questionBank.size-1
+            updateQuestion()
         }
     }
 
