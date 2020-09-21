4edad020f07f4664ac31dfd99e2f662c1bef2779
Sukaina Ahmed Yahya Al-Mutawakel <sukaina2099@gmail.com>
2020-09-21 11:07:46 +0300

    Challenge5

---
+++ app/src/main/java/com/example/androidquestion2third/CheckAnsweredQuestions.kt
@@ -0,0 +1,8 @@
+package com.example.androidquestion2third
+
+data class CheckAnsweredQuestions(val currentIndex:Int,var answered:Boolean)
+{
+
+
+
+}
--- app/src/main/java/com/example/androidquestion2third/MainActivity.kt
+++ app/src/main/java/com/example/androidquestion2third/MainActivity.kt
@@ -15,6 +15,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: ImageButton
     private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
+    private var AnswerBank= listOf(CheckAnsweredQuestions(0,false),
+        CheckAnsweredQuestions(1,false),
+        CheckAnsweredQuestions(2,false),
+        CheckAnsweredQuestions(3,false),
+        CheckAnsweredQuestions(4,false),
+        CheckAnsweredQuestions(5,false)
+
+    )
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
@@ -24,7 +32,8 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true)
     )
     private var currentIndex =0
-
+    private var  grade=0
+    private var numAnswer=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,23 +43,32 @@ class MainActivity : AppCompatActivity() {
         previousButton=findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
 
+
+
         trueButton.setOnClickListener {
-           checkAnswer(true)
+
+            AnswerBank[currentIndex].answered=true
+            checkAnswer(true)
+            isAnswered()
 
 
         }
         falseButton.setOnClickListener {
+            AnswerBank[currentIndex].answered=true
 
 
            checkAnswer(false)
+            isAnswered()
 
         }
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
 
         nextButton.setOnClickListener {
+
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
+            isAnswered()
 
         }
 
@@ -59,11 +77,35 @@ class MainActivity : AppCompatActivity() {
             currentIndex = (currentIndex - 1) % questionBank.size
             if(currentIndex<0)
                 currentIndex=questionBank.size-1
+
             updateQuestion()
+            isAnswered()
+
         }
     }
 
+    private fun isAnswered()
+    {
+        if( AnswerBank[currentIndex].answered==true)
+        {
+            trueButton.isEnabled=false
+            falseButton.isEnabled=false
+
+        }
+        else
+        {
+            trueButton.isEnabled=true
+            falseButton.isEnabled=true
+
+        }
+
+
+    }
+
     private fun checkAnswer(userAnswer: Boolean) {
+         numAnswer++
+
+
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId =
             if (userAnswer == correctAnswer) {
@@ -71,7 +113,17 @@ class MainActivity : AppCompatActivity() {
             } else {
                 R.string.incorrect_toast
             }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        if(messageResId==R.string.correct_toast)grade++
+
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+          if(numAnswer==questionBank.size) {
+
+
+              val toast= Toast.makeText(this, "Your grade is " + grade, Toast.LENGTH_LONG)
+              toast.setGravity(Gravity.CENTER,0,0)
+              toast.show()
+          }
+
     }
 
 
